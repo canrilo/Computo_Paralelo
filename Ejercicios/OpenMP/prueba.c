94721ec941967fc30d3c68a85175f1e8a4776260
canrilo <cam.rivera@gmail.com>
2015-06-19 23:35:34 -0500

    prueba de variables de entorno

--- Ejercicios/OpenMP/prueba.c
+++ Ejercicios/OpenMP/prueba.c
@@ -5,7 +5,7 @@ int	main(int argc, char* argv[])
 {
 	//Esta instrucción permite definir el número de procesadores
 	//omp_set_num_threads(8);
-	#pragma omp parallel /*O se puede poner directamente desde la directiva*/ num_threads(8)
+	#pragma omp parallel /*O se puede poner directamente desde la directiva*/ num_threads(4)
 	{
 		int	ID;
 		ID = omp_get_thread_num();
